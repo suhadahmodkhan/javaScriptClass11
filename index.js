4d33fabbe37eb5479a42dc1859952b4c23f2eaf5
suhadahmodkhan <suhadahmodkhan04@gmail.com>
2024-08-10 01:07:10 -0700

    main

---
+++ index.js
@@ -0,0 +1,13 @@
+const height = document.getElementById("height")
+const base = document.getElementById("base")
+const calculate = document.getElementById("calculate")
+const area = document.getElementById("area")
+
+
+calculate.addEventListener("click",function () {
+
+    console.log("calculate button clicked" , height.value , base.value)
+    let totalArea = 0.5 * height.value * base.value ;
+    console.log(totalArea)
+    area.innerText = totalArea ;
+})
